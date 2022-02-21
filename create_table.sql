fc3826c30b9106471ea727e0576fc82de91cc3b1
lee0916jh <lee0916jh@gmail.com>
2022-02-21 13:41:14 -0600

    sql query

--- create_table.sql
+++ create_table.sql
@@ -1,3 +1,7 @@
 CREATE TABLE Users (
     username VARCHAR(256) PRIMARY KEY
 );
+
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON * . * TO 'admin'@'localhost';
+FLUSH PRIVILEGES;
